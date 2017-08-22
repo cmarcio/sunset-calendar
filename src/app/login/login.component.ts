e033cf8da68877a29ac3809e22766365ed7122e9
Marcio <marciodscampos@gmail.com>
2017-08-22 16:39:32 -0300

    login/sigup forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-//import { Input } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { User } from './../classes/user';
 import { UserService } from './../user.service';
+
 
 @Component({
   selector: 'app-login',
@@ -10,20 +11,28 @@ import { UserService } from './../user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  //@Input() user: User;
   submitted = false;
   failed = false;
+  loginForm: FormGroup;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      username: new FormControl('', Validators.compose([Validators.required])),
+      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)]))
+    });
+  }
 
   login(username: string, password: string): void {
     this.userService
-      .getUser(username, password)
+      .get(username, password)
       .then( user => console.log(user))
       .catch( error => { this.failed = true; console.log("error: " + error);} );
   }
 
   onSubmit(){
     this.submitted = true;
+    this.failed = false;
+    if (this.loginForm.valid)
+      this.login(this.loginForm.get('username').value, this.loginForm.get('password').value);
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+
+import { User } from './../classes/user';
+import { UserService } from './../user.service';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
+  submitted = false;
+  failed = false;
+  signForm: FormGroup;
+  user = new User();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private userService: UserService, formBuilder: FormBuilder) {
+    this.signForm = formBuilder.group({
+      username: new FormControl('', Validators.compose([Validators.required])),
+      name: new FormControl('', Validators.compose([Validators.required])),
+      email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
+      confirm_password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)]))
+    }, {validator: this.passwordConfirming});
   }
+
+  passwordConfirming(c: AbstractControl): { invalid: boolean } {
+    if (c.get('password').value !== c.get('confirm_password').value) {
+        return {invalid: true};
+    }
+  }
+
+  signup(user: User): void {
+    this.userService
+      .create(user)
+      .then( user => console.log(user))
+      .catch( error => { this.failed = true; console.log("error: " + error);} );
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    this.failed = false;
+    if (this.signForm.valid){
+      this.user.username = this.signForm.get('username').value;
+      this.user.name = this.signForm.get('name').value;
+      this.user.password = this.signForm.get('password').value;
+      this.user.email = this.signForm.get('email').value;
+      this.signup(this.user);
+    }
+  }
+
 
 }
